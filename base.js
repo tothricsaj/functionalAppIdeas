2943a68f9f39d79bcf076091bba14f21d1685862
tothricsaj <tothricsaj@gmail.com>
2020-06-04 23:21:53 +0200

    base scss is added/FAIMOdal is implemented

--- base.js
+++ base.js
@@ -3,3 +3,13 @@ export const getInputValue = (inputElem) => {
     if(document.querySelector(inputElem).tagName !== 'INPUT') return undefined
     return document.querySelector(inputElem).value
 }
+
+export const createModal = (message) => {
+    let dom = `
+        <div class="FAIModalWrapper">
+            <div class="FAIModalMessage">${message} <span class="FAIModalClose">X</span></div>
+        </div>
+    `
+
+    document.body.insertAdjacentHTML('afterbegin', dom)
+}
--- bin2dec/index.js
+++ bin2dec/index.js
@@ -1,11 +1,11 @@
 import './style.scss'
 import {pipe} from '../trzFuncitonal.js'
-import {changeTextContent, getInputValue} from '../base.js'
+import {changeTextContent, getInputValue, createModal} from '../base.js'
 
 console.log('This is the bin2dec')
 
 const convertBtn = document.querySelector('.convertBtn')
-const checkBinaryInput = (input) => input.split('').every((val) => val === '0' || val === '1') ? input : undefined
+const checkBinaryInput = (input) => input.split('').every((val) => val === '0' || val === '1') ? input : createModal('This is not binary syntax!!! Please use only 1 or 0')
 
 const convertBin2Dec = (binVal) => parseInt(binVal, 2)
 const convertedDecValue = pipe(pipe(getInputValue, checkBinaryInput), convertBin2Dec)
