ee456d68b2adf09eb8bc683516dd3bae8c2a5486
tothricsaj <tothricsaj@gmail.com>
2020-06-04 15:59:01 +0200

    checking correct input from concrate input elem

--- bin2dec/index.js
+++ bin2dec/index.js
@@ -1,4 +1,5 @@
 import './style.scss'
+import {pipe} from '../trzFuncitonal.js'
 import {changeTextContent, getInputValue} from '../base.js'
 
 console.log('This is the bin2dec')
@@ -6,4 +7,6 @@ console.log('This is the bin2dec')
 const convertBtn = document.querySelector('.convertBtn')
 const checkBinaryInput = (input) => input.split('').every((val) => val === '0' || val === '1') ? input : undefined
 
-convertBtn.addEventListener('click', () => changeTextContent('.output', getInputValue('#binaryInput')))
+const checkCorrectInput = pipe(getInputValue, checkBinaryInput)
+
+convertBtn.addEventListener('click', () => changeTextContent('.output', checkCorrectInput('#binaryInput')))
