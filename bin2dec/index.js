b9975ad0db44a9c2c736604e32067d71aa23a032
tothricsaj <tothricsaj@gmail.com>
2020-06-09 14:40:30 +0200

    cursor is pointer on close/observer is removed

--- bin2dec/index.js
+++ bin2dec/index.js
@@ -18,29 +18,3 @@ const convertBin2Dec = (binVal) => parseInt(binVal, 2)
 const convertedDecValue = pipe(pipe(getInputValue, checkBinaryInput), convertBin2Dec)
 
 convertBtn.addEventListener('click', () => changeTextContent('.output', convertedDecValue('#binaryInput')))
-
-
-const targetNode = document.body
-
-// Options for the observer (which mutations to observe)
-const config = { attributes: true, childList: true, subtree: true };
-
-// Callback function to execute when mutations are observed
-const callback = function(mutationsList, observer) {
-    // Use traditional 'for loops' for IE 11
-    for(let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            console.log('A child node has been added or removed.');
-            clearValue()
-        }
-    }
-};
-
-// Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
-
-// Start observing the target node for configured mutations
-observer.observe(targetNode, config);
-
-// Later, you can stop observing
-// observer.disconnect();
