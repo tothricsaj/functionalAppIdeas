0fe03fb5583b70baf53f6ae2ee6233413c948fff
tothricsaj <tothricsaj@gmail.com>
2020-06-25 15:23:46 +0200

    id for input ranges

--- borderRadiusPrev/index.js
+++ borderRadiusPrev/index.js
@@ -1,3 +1,9 @@
 import './style.scss'
+import '../base.js'
+import { getInputValue } from '../base.js'
 
 console.log('Border Radius previewer')
+
+let topLeftX = getInputValue('#topLeftX')
+
+console.log(topLeftX)
